ed4b1bc3d726fd7eb9953c1708b43b2bf1809cf7
Valentin <renard.valentin@hotmail.fr>
2016-12-14 22:54:12 +0100

    send output completed on Virtual DeltaStar, todo progress and input process

--- Pigou/src/fr/deltastar/pigou/controller/SettingsAutoConfigController.java
+++ Pigou/src/fr/deltastar/pigou/controller/SettingsAutoConfigController.java
@@ -12,6 +12,7 @@ import fr.deltastar.pigou.service.ServicePigou;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -39,6 +40,7 @@ public class SettingsAutoConfigController extends BaseViewController implements 
     
     @FXML private ProgressBar pbOutput;
     @FXML private Label lNbOutput;
+    private int currentNbOutputArduino;
     private int currentNbOutput;
     private int totalNbOutput;
     
@@ -48,7 +50,7 @@ public class SettingsAutoConfigController extends BaseViewController implements 
     @FXML private Label lNbInput;
     private int currentNbInput;
     private int totalNbInput;
-    private Button btnNext;
+    @FXML private Button btnNext;
     
     @FXML private ProgressBar pbTotal;
     @FXML private Label lNbTotal;
@@ -68,7 +70,7 @@ public class SettingsAutoConfigController extends BaseViewController implements 
         this.totalNb = this.listComponentsInput.size() + this.listComponentsOutput.size();
         this.totalNbOutput = this.listComponentsOutput.size();
         
-        this.lNbOutput.setText(String.format(Constants.AUTOCONFIG_VIEW_NB_OUTPUT, this.currentNbOutput, this.totalNbOutput));
+        this.lNbOutput.setText(String.format(Constants.AUTOCONFIG_VIEW_NB_OUTPUT, this.currentNbOutputArduino, this.totalNbOutput));
         this.lNbInput.setText(String.format(Constants.AUTOCONFIG_VIEW_NB_INPUT, this.currentNbInput, this.totalNbInput));
         this.lNbTotal.setText(String.format(Constants.AUTOCONFIG_VIEW_NB_TOTAL, this.currentNbTotal, this.totalNb));
         
@@ -82,9 +84,11 @@ public class SettingsAutoConfigController extends BaseViewController implements 
      */
     @FXML
     public void btnNextClick() {
-        //TODO
-        //this.listComArduino.get(this.currentArduino).setValueLed(this.currentPosOutputArduino, ComponentConstants.OFF);
-        //this.listComArduino.get(this.currentArduino).sendOutput();
+        //TODO inscrire le numéro dans le composant choisi de la liste output
+        if (this.currentPosOutputArduino > 0) {
+            //remise a zero de l'output précédent
+            this.listComArduino.get(this.currentArduino).setValueLed(this.currentPosOutputArduino - 1, ComponentConstants.OFF);
+        }
         this.nextStep();
     }
 
@@ -94,7 +98,7 @@ public class SettingsAutoConfigController extends BaseViewController implements 
      */
     @Override
     public void onDataReceved(String data) {
-        //TODO
+        //TODO insérer le numéro 'data' dans le composant actuelle input de la liste 
         this.nextStep();
     }
     
@@ -104,25 +108,44 @@ public class SettingsAutoConfigController extends BaseViewController implements 
      */
     public void nextStep() {
         if (this.totalNb > this.currentNbTotal) {
-            if (this.totalNbOutput >= this.currentNbOutput) {
+            if (this.totalNbOutput - 2 > this.currentNbOutput) {
                 //PARAMETRAGE OUTPUT
                 //si on atteint la limite pour l'envoi des outputs sur l'arduino en cours, on change
                 if (this.currentPosOutputArduino > this.listComArduino.get(this.currentArduino).getNbOutputLed() - 1) {
-                    this.currentPosOutputArduino = 0;
                     this.currentArduino++;
+                    this.listComArduino.get(this.currentArduino - 1).setValueLed(this.currentPosOutputArduino - 1, ComponentConstants.OFF);
+                    this.listComArduino.get(this.currentArduino - 1).sendOutput();
+                    this.currentPosOutputArduino = 0;
                 }
                 this.listComArduino.get(this.currentArduino).setValueLed(this.currentPosOutputArduino, ComponentConstants.ON);
                 this.listComArduino.get(this.currentArduino).sendOutput();
                 
                 this.currentPosOutputArduino++;
+                this.currentNbOutputArduino++;
                 this.currentNbOutput++;
-                this.totalNbOutput++;
+                //TODO faire évoluer la progressbar output et total
             } else {
-                System.out.println("");
+                //remise a zéro de la précédente led output avant paramétrage input
+                if (this.currentNbOutputArduino > 0) {
+                    this.listComArduino.get(this.currentArduino).setValueLed(this.currentPosOutputArduino - 1, ComponentConstants.OFF);
+                    this.listComArduino.get(this.currentArduino).sendOutput();
+                    this.currentNbOutputArduino = 0;
+                    //changement affichage
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            //TODO plantage nummpointer lol
+                            btnNext.setVisible(false);
+                            pbInput.setVisible(true);
+                        }
+                    });
+                    this.nextStep();
+                    return;
+                }
+                System.out.println("input");
                 //PARAMETRAGE INPUT
-                this.btnNext.setVisible(false);
-                this.pbInput.setVisible(true);
-
+                //TODO bascule sur le composant suivant et demande a l'utilisateur de le presser
+                //TODO faire évoluer la progressbar input et total
                 this.currentNbInput++;
             }
         } else {
